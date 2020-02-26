8a28a3ab36a36817f50e7ace7dde971c55831c9e
martinparachu <51450877+martinparachu@users.noreply.github.com>
2020-02-26 16:40:47 -0300

    Add files via upload

---
+++ Assets/Scripts/Editor/PerformBuild.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+class BuildSrcipt
+{
+    static string[] scenes = { "Assets/Scenes/CSharpScene.unity" };
+    static string name = "HelloWorld";
+
+
+    static void BuildLinux()
+    {
+        BuildPipeline.BuildPlayer(scenes, "./" + name + "_Linux/" + name, BuildTarget.StandaloneLinux64, BuildOptions.None);
+    }
+
+}
